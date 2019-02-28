a807d44c2a6aaf0b2064107167eedea082a77325
Superdanby <dannymrt1@gmail.com>
2019-02-28 14:07:03 +0800

    First commit, Score: 56/100
    Got lots of ERROR: Failed to store removed value.

--- queue.c
+++ queue.c
@@ -26,7 +26,11 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* What if malloc returned NULL? */
-    q->head = NULL;
+    if (q) {
+        q->head = NULL;
+        q->tail = NULL;
+        q->size = 0;
+    }
     return q;
 }
 
@@ -35,7 +39,25 @@ void q_free(queue_t *q)
 {
     /* How about freeing the list elements and the strings? */
     /* Free queue structure */
-    free(q);
+    if (q) {
+        list_ele_t *current = q->head;
+        list_ele_t *next = NULL;
+        for (; current != NULL; current = next) {
+            if (current->value) {
+                free(current->value);
+                current->value = NULL;
+            }
+            next = current->next;
+            // current -> next = NULL;
+            free(current);
+        }
+        q->head = NULL;
+        q->tail = NULL;
+        q->size = 0;
+        free(q);
+        q = NULL;
+    }
+    return;
 }
 
 /*
@@ -49,11 +71,27 @@ bool q_insert_head(queue_t *q, char *s)
 {
     list_ele_t *newh;
     /* What should you do if the q is NULL? */
+    if (!q)
+        return false;
     newh = malloc(sizeof(list_ele_t));
+    if (!newh)
+        return false;
     /* Don't forget to allocate space for the string and copy it */
+    newh->value = malloc((strlen(s) + 1) * sizeof(char));
     /* What if either call to malloc returns NULL? */
+    if (!(newh->value)) {
+        free(newh);
+        newh = NULL;
+        return false;
+    }
+    strcpy(newh->value, s);
+    ++(q->size);
+    // Insert to head
     newh->next = q->head;
     q->head = newh;
+    // Check tail availability
+    if (!(q->tail))
+        q->tail = newh;
     return true;
 }
 
@@ -69,7 +107,29 @@ bool q_insert_tail(queue_t *q, char *s)
 {
     /* You need to write the complete code for this function */
     /* Remember: It should operate in O(1) time */
-    return false;
+    list_ele_t *newt;
+    // Check NULL for q
+    if (!q)
+        return false;
+    newt = malloc(sizeof(list_ele_t));
+    if (!newt)
+        return false;
+    newt->value = malloc((strlen(s) + 1) * sizeof(char));
+    if (!(newt->value)) {
+        free(newt);
+        newt = NULL;
+        return false;
+    }
+    strcpy(newt->value, s);
+    newt->next = NULL;
+    ++(q->size);
+    // Insert to tail
+    if (q->tail)
+        q->tail->next = newt;
+    else  // No elements in q
+        q->head = newt;
+    q->tail = newt;
+    return true;
 }
 
 /*
@@ -83,7 +143,26 @@ bool q_insert_tail(queue_t *q, char *s)
 bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
     /* You need to fix up this code. */
+    // Check q and q -> head availability
+    if (!q || !(q->head))
+        return false;
+    list_ele_t *temp = q->head;
+    // Check sp and bufsize availability
+    if (!sp && (temp->value) && bufsize) {
+        strncpy(sp, temp->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+        // Release value memory
+        free(temp->value);
+        temp->value = NULL;
+    }
+    // Remove head
     q->head = q->head->next;
+    // temp -> next = NULL;
+    free(temp);
+    temp = NULL;
+    // Check if list is empty
+    if (!(--(q->size)))
+        q->tail = NULL;
     return true;
 }
 
@@ -95,6 +174,8 @@ int q_size(queue_t *q)
 {
     /* You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
+    if (q)
+        return q->size;
     return 0;
 }
 
@@ -108,4 +189,16 @@ int q_size(queue_t *q)
 void q_reverse(queue_t *q)
 {
     /* You need to write the code for this function */
+    // Check q availability
+    if (!q || (q->size) <= 1)
+        return;
+    list_ele_t *prev = NULL;
+    list_ele_t *current = q->tail = q->head;
+    list_ele_t *next = current->next;
+    for (; next != NULL; prev = current, current = next, next = current->next) {
+        current->next = prev;
+    }
+    current->next = prev;
+    q->head = current;
+    return;
 }
