350103347a75144e75d1e19a68420cf9a0703b41
Superdanby <dannymrt1@gmail.com>
2019-02-28 14:15:44 +0800

    Correct NULL detection statement for sp
    Score: 93/100

--- queue.c
+++ queue.c
@@ -148,7 +148,7 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
         return false;
     list_ele_t *temp = q->head;
     // Check sp and bufsize availability
-    if (!sp && (temp->value) && bufsize) {
+    if (sp && (temp->value) && bufsize) {
         strncpy(sp, temp->value, bufsize - 1);
         sp[bufsize - 1] = '\0';
         // Release value memory
